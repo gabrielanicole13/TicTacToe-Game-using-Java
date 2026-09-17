3ca0a40002a5ebd31b9da40248b233a44a018ae0
Gabriela Dacayana <gabrieladacayana@gmail.com>
2026-09-17 20:49:11 +0800

    Add GameBoard and GameLogic files

---
+++ src/GameLogic.java
@@ -0,0 +1,83 @@
+import java.util.Arrays;
+
+public class GameLogic {
+
+    int row = 3;
+    int col = 3;
+    char[][] board = new char[row][col];
+    char currentPlayer = 'X';
+
+    public GameLogic(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+
+    public void placeMark(int r, int c){
+        if(currentPlayer == 'X'){
+            board[r][c] = 'X';
+        }else{
+            board[r][c] = 'O';
+        }
+    }
+
+    public boolean isCellEmpty(int r, int c){
+        if(board[r][c] == ' '){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean checkLine(char a, char b, char c){
+        return a != ' ' && a == b && b == c;
+    }
+
+    public boolean checkWin(){
+        for(int i = 0; i < 3; i++){
+            if(checkLine(board[i][0], board[i][1], board[i][2])) return true;
+
+            if(checkLine(board[0][i], board[1][i], board[2][i])) return true;
+        }
+        if(checkLine(board[0][0], board[1][1], board[2][2])) return true;
+        if(checkLine(board[0][2], board[1][1], board[2][0])) return true;
+
+        return false;
+    }
+
+    public boolean isDraw(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == ' '){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void switchTurn(){
+        if(this.currentPlayer == 'X'){
+            currentPlayer = 'O';
+        }else{
+            currentPlayer = 'X';
+        }
+    }
+
+    public char getCurrentPlayer(){
+        return currentPlayer;
+    }
+
+    public void printBoard(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,39 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        GameLogic logic = new GameLogic();
+
+        while (logic.checkWin() != true && logic.isDraw() != true) {
+            logic.printBoard();
+            System.out.print("Input the row (0,1,2) for the desire placement: ");
+            int row = input.nextInt();
+            System.out.print("Input the column(0,1,2) for the desire placement: ");
+            int col = input.nextInt();
+
+            if(logic.isCellEmpty(row, col)){
+                logic.placeMark(row, col);
+            }else{
+                System.out.println("You have placed an invalid mark, so u lose a turn");
+            }
+
+
+
+            if(logic.checkWin()){
+                logic.printBoard();
+                System.out.println("Congratulations, " + logic.getCurrentPlayer() + " wins the game");
+            }
+
+            logic.switchTurn();
+
+            if(logic.isDraw()){
+                logic.printBoard();
+                System.out.println("Game is draw");
+            }
+
+        }
+
+    }
 
 }
